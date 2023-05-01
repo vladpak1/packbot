494ea241acb16875c2004b5af49937d2869d975b
vladpak1 <admin@mult34.com>
2023-05-01 08:23:52 +0700

    add cronjob_daily.php

--- cronjob_daily.php
+++ cronjob_daily.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * Query this file to do job.
- * Run this file every minute or so.
+ * Query this file to do daily job.
  */
 require_once __DIR__ . '/vendor/autoload.php';
 if ($_GET['key'] !== PackBot\Environment::var('cronjob_key')) die('Invalid key.');
@@ -10,4 +9,4 @@ if ($_GET['key'] !== PackBot\Environment::var('cronjob_key')) die('Invalid key.'
 $worker = new PackBot\Worker();
 
 
-$worker->doJob();
+$worker->doDailyJob();
