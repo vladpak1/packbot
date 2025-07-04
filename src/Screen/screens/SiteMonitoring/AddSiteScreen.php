6fd13aab0b62fd5fbca8410ead27ca13931eaf38
vladpak1 <admin@mult34.com>
2025-07-04 15:53:22 +0700

    Массовое добавление сайтов

--- src/Screen/screens/SiteMonitoring/AddSiteScreen.php
+++ src/Screen/screens/SiteMonitoring/AddSiteScreen.php
@@ -88,12 +88,25 @@ final class AddSiteScreen extends Screen implements ScreenInterface
             case 0:
                 $notes['state'] = 1;
                 $conversation->update();
+                $siteManager  = new SiteManager($this->getUserID());
+                $currentQuota = Environment::var('monitoring_settings')['maxSitesPerUser'] - count($siteManager->getSites());
 
-                return $this->maybeSideExecute([
-                    'Отправьте сайт, чтобы добавить его в список мониторинга.',
-                ], $this->keyboard, true, [
+                return $this->maybeSideExecute(
+                    $this->text->concatEOL(
+                        'Отправьте сайт или список сайтов, чтобы добавить его в список мониторинга.',
+                        '',
+                        $this->text->sprintf('Каждый сайт должен быть на отдельный строке. Вы можете добавить еще %d сайтов.', $currentQuota),
+                        '',
+                        'Может потребоваться внести в белый список нашего бота, чтобы он смог проверять ваш сайт. Вы можете узнать его:',
+                        'По useragent: <code>PackBot/2.0 (+https://t.me/packhelperbot)</code>',
+                        'По IP-адресу: <code>212.162.153.137</code>'
+                    ),
+                    $this->keyboard,
+                    false,
+                    [
                     'parse_mode' => 'HTML',
-                ]);
+                ]
+                );
             case 1:
                 return $this->proccessCheckRequest($conversation);
             default:
@@ -135,52 +148,107 @@ final class AddSiteScreen extends Screen implements ScreenInterface
         ], 2);
     }
 
-    private function proccessCheckRequest($conversation)
+    private function proccessCheckRequest(Conversation $conversation)
     {
 
         $message = $this->getText();
 
-        if (empty($message) || count(explode(PHP_EOL, $message)) > 1 || !Url::isValid($message) || !str_contains($message, '.') || str_contains($message, ' ')) {
-            return $this->tryToSendMessage($this->text->e('Некорректный сайт. Пожалуйста, убедитесь, что вы отправили только один корректный адрес сайта и попробуйте еще раз.'));
+        $sites = explode(PHP_EOL, $message);
+
+        if (0 === count($sites) || !str_contains($message, '.')) {
+            return $this->tryToSendMessage($this->text->e('Неправильный формат. Вы должны отправить один корректный сайт или список сайтов - каждый с новой строки. Попробуйте еще раз.'));
+        }
+
+        $sites = array_unique(array_map('trim', array_filter($sites)));
+
+        if (count($sites) > Environment::var('monitoring_settings')['maxSitesPerUser']) {
+            return $this->tryToSendMessage(
+                $this->text->sprintf(
+                    'Вы можете добавить не более %d сайтов в мониторинг. Попробуйте еще раз, сократив список до этого количества.',
+                    Environment::var('monitoring_settings')['maxSitesPerUser']
+                )
+            );
         }
 
         $conversation->stop();
+        $success = [];
+        $error   = [];
 
-        $response = $this->tryToSendMessage($this->text->e('Добавляю сайт... Это может занять некоторое время.'), false);
-        sleep(1);
-        $reportMessageID = $response->getResult()->getMessageId();
+        foreach ($sites as $key => $site) {
+            if (!Url::isValid($site)) {
+                unset($sites[$key]);
+                $error[] = [
+                    'site'   => $site,
+                    'reason' => $this->text->e('Некорректный адрес сайта.'),
+                ];
+
+            }
+        }
+
+        $response          = $this->tryToSendMessage($this->text->e('Добавляю сайты... Это может занять некоторое время.'));
+        $responseMessageID = $response->getResult()->getMessageId();
 
         $siteManager = new SiteManager($this->getUserID());
 
-        try {
-            $siteManager->addSite(trim(mb_strtolower($message)));
-        } catch (InvalidDomainException $e) {
-            return Request::editMessageText([
-                'chat_id'      => $this->isSideExecute() ? $this->command->getCallbackQuery()->getFrom()->getId() : $this->command->getMessage()->getChat()->getId(),
-                'message_id'   => $reportMessageID,
-                'text'         => $this->text->e('Некорректный сайт. Пожалуйста, убедитесь, что вы отправили только один корректный адрес сайта и попробуйте еще раз.') . ' ' . $e->getMessage(),
-                'parse_mode'   => 'HTML',
-                'reply_markup' => $this->afterKeyboard,
-            ]);
-        } catch (SiteMonitoringException $e) {
-            return Request::editMessageText([
-                'chat_id'      => $this->isSideExecute() ? $this->command->getCallbackQuery()->getFrom()->getId() : $this->command->getMessage()->getChat()->getId(),
-                'message_id'   => $reportMessageID,
-                'text'         => $this->text->sprintf('Не удалось добавить сайт. Причина: %s', $this->text->e($e->getMessage())),
-                'parse_mode'   => 'HTML',
-                'reply_markup' => $this->afterKeyboard,
-            ]);
+        foreach ($sites as $site) {
+            try {
+                $result = $siteManager->addSite($site);
+
+                if ($result) { // get last site
+                    $usersSites = $siteManager->getSites();
+                    /**
+                     * @var Site $newSite
+                     */
+                    $newSite   = end($usersSites);
+                    $success[] = $newSite->getURL();
+                } else {
+                    throw new \Exception('Неизвестная ошибка базы данных.');
+                }
+            } catch (\Throwable $e) {
+                $error[] = [
+                    'site'   => $site,
+                    'reason' => $e->getMessage(),
+                ];
+            }
+
         }
-        sleep(5);
-        Request::editMessageText([
-            'chat_id'                  => $this->isSideExecute() ? $this->command->getCallbackQuery()->getFrom()->getId() : $this->command->getMessage()->getChat()->getId(),
-            'message_id'               => $reportMessageID,
-            'text'                     => $this->text->sprintf('Сайт %s успешно добавлен в мониторинг.', Url::getEffectiveUrl($message)),
-            'parse_mode'               => 'HTML',
+        $successMessage = '';
+
+        if (count($success) > 0) {
+            $successMessage = $this->text->sprintf('%d сайтов успешно добавлено в мониторинг:', count($success));
+
+            foreach ($success as $site) {
+                $successMessage .= PHP_EOL . '✅ ' . $site;
+            }
+        }
+
+        $errorMessage = '';
+
+        if (count($error) > 0) {
+            $errorMessage = PHP_EOL . $this->text->e('<b>Некоторые сайты не удалось добавить в мониторинг:</b>');
+
+            foreach ($error as $item) {
+                $errorMessage .= PHP_EOL . $this->text->sprintf(
+                    '⚠️ %s - %s',
+                    $item['site'],
+                    $item['reason']
+                );
+            }
+        }
+
+        return Request::editMessageText([
+            'chat_id'    => $this->isSideExecute() ? $this->command->getCallbackQuery()->getFrom()->getId() : $this->command->getMessage()->getChat()->getId(),
+            'message_id' => $responseMessageID,
+            'text'       => $this->text->concatEOL(
+                $successMessage,
+                $errorMessage,
+                '',
+                'Вы можете добавить еще сайты или вернуться к списку уже добавленных.',
+            ),
             'reply_markup'             => $this->afterKeyboard,
+            'parse_mode'               => 'HTML',
             'disable_web_page_preview' => true,
         ]);
 
-        return Request::emptyResponse();
     }
 }
